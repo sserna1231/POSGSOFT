cbb3dd880fc6535a2cb317eed1d073cce66703a0
Santiago Gamboa <santigamboa@javerianacali.edu.co>
2021-03-19 18:53:05 -0500

    Added: estudiante, source and header file

---
+++ include/estudiante.h
@@ -0,0 +1,26 @@
+#ifndef ESTUDIANTE_H
+#define ESTUDIANTE_H
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Estudiante{
+    private:
+        string nombre;
+        int id;
+        int celular;
+        string email;
+
+    public:
+        Estudiante( int );
+        Estudiante( string, int, int, string );
+        void llenarEstudiante( int );
+        void mostrarEstudiante();
+        int getId();
+}
+
+#endif
---
+++ source/estudiante.cpp
@@ -0,0 +1,51 @@
+#include "estudiante.h"
+
+Estudiante::Estudiante( int id ){
+    this->id = id;
+}
+
+Estudiante::Estudiante( string nombre, int id, int celular, string email ){
+    this->nombre = nombre;
+    this->id = id;
+    this->celular = celular;
+    this->email = email;
+}
+
+void Estudiante::llenarEstudiante( int id ){
+    string nombre, email;
+    int celular;
+
+    cout << endl;
+
+    cout << "Ingrese el nombre del estudiante: ";
+    cin.ignore();
+    getline( cin, nombre );
+
+    cout << "Telefono celular: ";
+    cin >> celular;
+
+    cout << "Ingrese el email: ";
+    cin >> email;
+
+    this->nombre = nombre;
+    this->id = id;
+    this->celular = celular;
+    this->email = email;
+
+    cout << endl;
+}
+
+void Estudiante::mostrarEstudiante(){
+    cout << endl;
+
+    cout << "* Nombre: " << this->nombre << endl;
+    cout << "* id: " << this->id << endl;
+    cout << "* Celular: " << this->celular << endl;
+    cout << "* Email: " << this->email << endl;
+
+    cout << endl;
+}
+
+int Colaborador::getId(){
+    return this->id;
+}
